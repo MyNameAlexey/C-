9912cd5c26b382014980d76dec36cd127c8d6fe7
Alexey Ponomarev <denver0-1986@mail.ru>
2023-03-12 20:12:46 +0300

    domashnie zadachi 64, 66, 68

---
+++ zadacha66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Sum (int numberM, int numberN)
+{
+    if (numberM == numberN)
+    {
+        return numberM;
+    }
+    return numberN + Sum(numberM, numberN - 1);
+}
+
+System.Console.Write("Введите минимальное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите максимальное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{Sum(numberM, numberN)}");
--- zadacha68/Program.cs
+++ zadacha68/Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных 
+// числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
 int Acer(int m, int n)
 {
     if(m == 0)
@@ -22,4 +26,3 @@ int numberM = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 Console.Write($"{Acer(numberM, numberN)}");
-
